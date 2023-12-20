c67c42d4c4e48a27b96f3d6353a6c5d8bb29235b
Altie <altielynn@gmail.com>
2023-12-19 18:45:09 -0700

    please just work

---
+++ build.bat
@@ -0,0 +1,16 @@
+@REM #!/bin/bash
+set -eu
+
+@REM # For use in the GitHub workflow process.
+@REM # Run in the root of the repository to produce a dist/ directory.
+
+@REM # Create directory structure
+mkdir -p dist
+
+@REM # Copy fonts and images
+cp -a fonts images dist
+
+@REM # Copy and minify CSS
+mkdir -p dist/css
+cp sigma.css dist/css
+npm run minify
